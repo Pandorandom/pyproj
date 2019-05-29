840fe1309a40c0d1a7f70de81a80c4e8c7c52fd0
Taylor Acree <taylor.acree@local>
2019-05-28 21:07:27 -0600

    Update README, add base imports/some code to main.py

--- main.py
+++ main.py
@@ -2,12 +2,22 @@
 #
 #
 #
-# TOOL NAME:
+# TOOL NAME: Pyproj
 # WRITTEN BY: tacree
-# DATE:
+# DATE: 05-28-2019
 # REV:
-# First Worked:
-# Purpose:
+# First Worked: 05-28-2019
+# Purpose: The purpose of this script is to offer a project creator application written in python over the original tool (bash).
+
+# Style Notes (W.I.P. Idead for a section):
+#
+# Use camelCase notation for variables, etc.
+#
+#
+#
+#
+
+
 #
 # REV LIST:
 # BY:
@@ -18,35 +28,54 @@
 #
 
 #############################################################
-# Section 1: Functions
+# Section 1: Imports
 #############################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import os, getpass, time, socket, sys, Tkinter
+from github import Github
 
 
 
 
 
 #############################################################
-# Section 2: Main Body
+# Section 2: Misc. Variables
 #############################################################
+username = getpass.getuser()
+homeDirectory = os.path.expanduser("~")
+hostname = socket.gethostname()
+
+
+#############################################################
+# Section 3: Functions
+#############################################################
+
+def checkProfile():
+	bashProfile = '%s/.bash_profile' % homeDirectory
+	check = os.path.exists(bashProfile)
+	if check == True:
+		bashProfile = '%s/.bash_profile' % homeDirectory
+	elif check == False:
+		bashProfile = '%s/.bashrc' % homeDirectory
+		check2 = os.path.exists(bashProfile)
+		if check2 == False:
+			print("%s/.bash_profile and %s/.bashrc not found.")
+			bashProfile = raw_input("Please indicate the location of your bash profile: ")
+	else:
+		print(usage)
+
+
+def usage():
+	print '''
+	This script will create a new project directory in a target directory and offer the user the option to open a new tab of the directory as well as open the project folder in Atom Text editor.
+'''
+
+#############################################################
+# Section 4: Main Body
+#############################################################
+
+alias = raw_input("Do you want to create an alias in ~/.bash_profile?")
+
+if alias = "y":
+	bashProfile = open("~/.bash_profile","w+")
+	if bashProfile:
+		pass
