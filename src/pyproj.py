f1ee81ee1e9bc46669f7212ab2106a4a236f8091
Taylor Acree <taylor.acree@local>
2019-05-30 14:43:20 -0600

    Pyproj actually does things

--- src/pyproj.py
+++ src/pyproj.py
@@ -7,7 +7,7 @@
 # DATE: 05-28-2019
 # REV:
 # First Worked: 05-28-2019
-# Purpose: The purpose of this script is to offer a project creator application written in python over the original tool (bash).
+# Purpose: The purpose of this script is to offer a project creator application written in python over the original tool (bash). Current set up is to have everything created to ~/projects/..
 
 ##########################################
 # Style Notes (W.I.P. Idea for a section):
@@ -40,7 +40,7 @@
 import os, getpass, time, socket, sys
 from github import Github
 from Tkinter import *
-
+from os import system, name
 
 
 
@@ -50,9 +50,6 @@ from Tkinter import *
 #############################################################
 # get system username
 username = getpass.getuser()
-
-# expands symlink of /home directory
-homeDirectory = os.path.expanduser("~")
 
 # Get hostname of machine - for git commit tracking in the future
 hostname = socket.gethostname()
@@ -87,33 +84,60 @@ def usage():
 	return;
 
 # Run git init on newly created project and sub files. Can also add a remote repository (github API to come one day, to allow for repo creation via the tool)
-def gitInit( str ):
+def gitInit(homeDirectory, projectName):
 	initRepo = 'cd %s/projects/%s && git init' % (homeDirectory, projectName)
 	doGit = raw_input("Would you like to git init this new project to begin tracking code changes? (y|n): ")
-	addRemote = 'cd %s/projects/%s && git git remote add origin %s' % (homeDirectory, projectName, remoteRepo)
 
 	if doGit == "y":
 		os.system(initRepo)
 	elif doGit == "n":
 		print("Did NOT git init the new project.")
-		return;
+		cd2Proj = raw_input("Would you like to switch to the new project directory? (y|n): ")
+		if cd2Proj == "y":
+			os.system('ttab cd %s/projects/%s/' % (homeDirectory, projectName))
+			sys.exit()
+		elif cd2Proj =="n":
+			print("Exiting pyproj creator.")
+			sys.exit()
+		else:
+			print("Oops, this is a yes or no question. Please enter y or n.")
 	else:
 		print("Oops, this is a yes or no question. Please enter y or n.")
 
-	# HTTPS or SSH git clone works. In the future when using the GitHub API, I have gotten https via api access token working, will need to review SSH settings to see if it's possible.
+	# HTTPS or SSH git clone works. In the future when using the GitHub API, I have gotten https via api access token working, will need to review SSH settings to see how to accomplish the same task.
 	remoteRepo = raw_input("Do you have a remote repository to add a url for? (y|n): ")
 	if remoteRepo == "y":
-		os.system(addRemote)
+		repoUrl = raw_input("Please enter the URL of the remote repository: ")
+		os.system('cd %s/projects/%s && git remote add remote %s' % (homeDirectory, projectName, repoUrl))
+		print("Remote repository added as 'remote'.")
+		cd2Proj = raw_input("Would you like to switch to the new project directory? (y|n): ")
+		if cd2Proj == "y":
+			os.system('ttab cd %s/projects/%s/' % (homeDirectory, projectName))
+			sys.exit()
+		elif cd2Proj =="n":
+			print("Exiting pyproj creator.")
+			sys.exit()
+		else:
+			print("Oops, this is a yes or no question. Please enter y or n.")
 	elif remoteRepo == "n":
 		print ("No remote repository added to new repository.")
-		return;
-	else:
-		print("Oops, this is a yes or no question. Please enter y or n.")
-
+		cd2Proj = raw_input("Would you like to switch to the new project directory? (y|n): ")
+		if cd2Proj == "y":
+			os.system('ttab cd %s/projects/%s/' % (homeDirectory, projectName))
+			sys.exit()
+		elif cd2Proj =="n":
+			print("Exiting pyproj creator.")
+			sys.exit()
+		else:
+			print("Oops, this is a yes or no question. Please enter y or n.")
 	return;
 
-# Made a little different structure for php applications. This function will take projectType, homeDirectory, and projectName to create a new project directory.
-def makeProj( projectType, homeDirectory, projectName ):
+# Copies template files to homeDirectory/projects/projectName, offers to open a terminal tab and atom text editor.
+def makeProj():
+	projectName = raw_input("Enter the name of your new project: ")
+	# expands symlink of /home directory
+	homeDirectory = os.path.expanduser("~")
+	#Based on some research. php projects have a little different structure. A lot of other languages employ/can be adapted to a structure as included with this tool.
 	isPhp = raw_input("Is this a php application? (y|n): ")
 	if isPhp == "y":
 		projectType = "php"
@@ -121,37 +145,41 @@ def makeProj( projectType, homeDirectory, projectName ):
 		projectType = "mainTemplate"
 	else:
 		print("Please enter y for yes if this is a PHP application, or n for no if it is another language.")
+		return makeProj()
 
-	makeProj = 'cp -R /resources/templates/%s %s/projects/%s' % (projectType, homeDirectory, projectName)
-	cdProj = 'cd %s/projects/%s' % (homeDirectory, projectName)
-	chMod = 'chmod 755 %s/projects/%s/src/* && chmod 755 %s/projects/%s/bin/*' % (homeDirectory, projectName)
-	ttabProj = 'ttab cd %s/projects/%s/' % (homeDirectory, projectName)
-	openAtom = 'atom %s/projects/%s/' % (homeDirectory, projectName)
+	currentDir = os.getcwd()
 
-	os.system(makeProj)
-	os.system(cdProj)
-	os.system(chMod)
-	openProj = raw_input("Open the new project directory and Atom text editor? (y|n): ")
+	os.system('mkdir %s/projects/%s' % (homeDirectory, projectName))
+	os.system('cp -R %s/src/lib/templates/%s/ %s/projects/%s' % (currentDir, projectType, homeDirectory, projectName))
+	os.system('cd %s/projects/%s && chmod 755 %s/projects/%s/src/ && chmod 755 %s/projects/%s/bin/' % (homeDirectory, projectName, homeDirectory, projectName, homeDirectory, projectName))
+
+	openProj = raw_input("Open the new project in Atom text editor? (y|n): ")
 	if openProj == "y":
-		os.system(ttabProj)
-		os.system(openAtom)
+		#os.system('ttab cd %s/projects/%s/' % (homeDirectory, projectName))
+		os.system('atom %s/projects/%s/' % (homeDirectory, projectName))
+		gitInit(homeDirectory, projectName)
 	elif openProj == "n":
 		print('Project folder created at %s/projects/%s/' % (homeDirectory, projectName))
+		gitInit(homeDirectory, projectName)
 	else:
 		print("Y or n, only, please.")
-
 	return;
 
+	gitInit(homeDirectory, projectName)
 
+def clear():
+
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 
 #############################################################
 # Section 4: Main Body
 #############################################################
-
-projectLanguage = raw_input("Enter the language of your new project: ")
-projectName = raw_input("Enter the name of your new project: ")
-
-
-makeproj( projectType = '%s', homeDirectory = '%s', projectName = '%s' % (projectType, homeDirectory, projectName) )
-gitInit()
+clear()
+makeProj()
